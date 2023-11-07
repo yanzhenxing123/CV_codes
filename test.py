47e028bf28df11dd64f73b75fc9303522aeb7f01
yanzhenxing123 <209243446@qq.com>
2023-11-07 14:45:53 +0800

    feat: add utils

--- test.py
+++ test.py
@@ -1,13 +1,41 @@
-import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+from skimage import io, color
 
-# 读取带有四通道的图片
-rgba_image = cv2.imread('car.png', cv2.IMREAD_UNCHANGED)
-print(rgba_image)
 
-# 提取RGB通道
-rgb_image = rgba_image[:, :, :3]
+def normalized_cut(image):
+    # 构建相似矩阵W
+    W = np.zeros((image.size, image.size))
+    sigma = 1.0
+    for i in range(image.size):
+        for j in range(i + 1, image.size):
+            diff = np.linalg.norm(image[i] - image[j])
+            W[i, j] = np.exp(-diff / sigma)
+            W[j, i] = W[i, j]
 
-print(rgb_image)
+    # 构建度矩阵D和拉普拉斯矩阵L
+    D = np.diag(np.sum(W, axis=1))
+    L = D - W
 
-# 保存为三通道的图片
-cv2.imwrite('output_image.png', rgb_image)
+    # 计算归一化割矩阵N
+    D_sqrt_inv = np.diag(1.0 / np.sqrt(np.sum(W, axis=1)))
+    N = D_sqrt_inv.dot(L).dot(D_sqrt_inv)
+
+    # 谱聚类
+    k = 2  # 聚类数目
+    eigvals, eigvecs = np.linalg.eig(N)
+    eigvecs = eigvecs[:, eigvals.argsort()[:k]]  # 取前k个特征向量
+    kmeans = KMeans(n_clusters=k).fit(eigvecs)
+
+    # 根据聚类结果分割图像
+    seg = np.zeros_like(image)
+    seg[kmeans.labels_ == 0] = 1
+    seg = seg.reshape(image.shape[:2])
+
+    return seg
+
+
+if __name__ == '__main__':
+    img = io.imread('imgs/task04/train.jpg')
+    gray_img = color.rgb2gray(img)
+    normalized_cut(gray_img)
---
+++ utils.py
@@ -0,0 +1,37 @@
+"""
+@Time : 2023/11/7 14:28
+@Author : yanzx
+@Description : 工具类
+"""
+
+import os
+from PIL import Image
+import os.path
+import glob
+
+
+def rename_img():
+    path = 'imgs/task04/car_raw'
+    files = os.listdir(path)  # 文件夹里的所有文件名存成列表list
+    for i, file in enumerate(files):
+        # 重点在05d，这样会自动补齐5位，不足的补零
+        # 为啥是0 + i，方便后面添加，把0改了就行
+        NewFileName = os.path.join(path, 'car%02d' % (1 + i) + '.png')
+        OldFileName = os.path.join(path, file)
+        print('第%d个文件：%s' % (i + 1, NewFileName))
+        os.rename(OldFileName, NewFileName)  # 改名
+
+
+def convertjpg(jpgfile, outdir, width=128*3, height=128*2):
+    img = Image.open(jpgfile)
+    try:
+        new_img = img.resize((width, height), Image.BILINEAR)
+        new_img.save(os.path.join(outdir, os.path.basename(jpgfile)))
+    except Exception as e:
+        print(e)
+
+
+if __name__ == '__main__':
+    # rename_img()
+    for jpgfile in glob.glob("imgs/task04/car_raw/*.png"):
+        convertjpg(jpgfile, "imgs/task04/car_raw2")
